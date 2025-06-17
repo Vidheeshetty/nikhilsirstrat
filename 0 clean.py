e83f09b7580b49e1b407ede1b6678082b9e395d3
synaptic-algos <synaptic.algos@gmail.com>
2025-06-17 08:32:58 +0530

    Initial commit on development branch

---
+++ 0 clean.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# Command to run
+command = ["dot_clean", "-m", "/Volumes/T7/Code Repositories/Repositories/NTbasedPlatform"]
+
+try:
+    result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print("Success:\n", result.stdout)
+except subprocess.CalledProcessError as e:
+    print("Error:\n", e.stderr)
